53c2ee230f6e0fd3addf3f1e785bceba52741a4d
Ricardo Gomes <ricardo@heptasoft.com>
2024-10-03 12:56:43 +0100

    Completes example

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,8 @@
 import {describe, test, expect } from "@jest/globals"
-import {sum} from '../src';
+import {handler} from "./index"
 
 describe('index', () => {
 	test('basic', async () => {
-		test('Returns correct value', () =>{
-			expect(sum(2, 3)).toEqual(5)
-		})
+		expect(await handler(undefined)).toEqual('Hello World!')
 	});
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,6 @@
 
 // Lambda Handler
 export async function handler(event) {
-	console.log('Hello World!');
-}
-
-export function sum(num1: number, num2: number): number {
-	return num1 + num2;
+	console.log("event %j", event);
+	return 'Hello World!';
 }
